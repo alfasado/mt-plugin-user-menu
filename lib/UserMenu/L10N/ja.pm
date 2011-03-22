9b4922dcfed5e20d56f08008528e865571665867
Alfasado Inc <webmaster@alfasado.jp>
2011-03-22 15:11:37 +0900

    first commit

---
+++ lib/UserMenu/L10N/ja.pm
@@ -0,0 +1,10 @@
+package UserMenu::L10N::ja;
+use strict;
+use base 'UserMenu::L10N';
+use vars qw( %Lexicon );
+
+our %Lexicon = (
+    'Show System Menu on Your Dashboard.' => 'ユーザーダッシュボードにシステムメニューを表示します。',
+);
+
+1;
